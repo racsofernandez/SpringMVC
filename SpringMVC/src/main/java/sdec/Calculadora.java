eb9c18bfab9832d0a852fb0670fa125e10bb6a73
Oscar Fernandez <oscar.fernandez4.contractor@bbva.com>
2013-09-10 17:13:42 +0200

    Se realiza una modificación mínima desde Eclipse en el trabajo

---
+++ SpringMVC/src/main/java/sdec/Calculadora.java
@@ -0,0 +1,14 @@
+package sdec;
+
+public class Calculadora {
+
+	public Calculadora() {
+		System.out.println("Calculadora inicializada por Spring !!");
+	}
+
+	public int sumar(int numero1, int numero2) {
+		System.out.println("Se realiza el cálculo de la suma a través de la clase Calculadora");
+		return numero1 + numero2;
+	}
+
+}
